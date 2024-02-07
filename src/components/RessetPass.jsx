3e3c7957bef2c4b171bc8cbb0ad21e3fd790453a
Fedor Drozdov <31051998fd@gmail.com>
2024-02-07 15:52:02 +0400

    без бордера

--- src/components/RessetPass.jsx
+++ src/components/RessetPass.jsx
@@ -12,10 +12,10 @@ export default function RessetPass (){
     return(
         <div>
             <ModalWindow>
-                <div className="ResetPass_nameComp">Востановление пароля</div>
-                <BaseInput className='ResetPass_BaseInput'placeholder='@reliab.tech'/>
-                <BaseInput className='ResetPass_BaseInput' placeholder='Новый пороль' SufixIcon={SufixIcon}/>
-                <BaseInput className='ResetPass_BaseInput' placeholder='Подтвердите пороль' SufixIcon={SufixIcon}/>
+                <div className="ResetPass_nameComp">Восстановление пароля</div>
+                <BaseInput className='ResetPass_BaseInput'placeholder='example@reliab.tech'/>
+                <BaseInput className='ResetPass_BaseInput' placeholder='Новый пароль' SufixIcon={SufixIcon}/>
+                <BaseInput className='ResetPass_BaseInput' placeholder='Подтвердите пароль' SufixIcon={SufixIcon}/>
                 <BaseButton  typeStyle='dark' className="ResetPass_button" >Сменить пароль</BaseButton>
 
                 <Logo className="ResetPass_logo"/>
